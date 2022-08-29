cedb4b7b5cb21835a9fcbe14201441ceb1ab4e96
teawon <xodnjsvkvk9@gmail.com>
2022-08-29 21:48:17 +0900

    chore : init Setting

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Mainpage from "./pages/Mainpage";
+import React from "react";
+function App() {
+  return (
+    <div>
+      <Mainpage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Mainpage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Mainpage() {
+  return (
+    //실제 화면에 출력할 내용들 설정 해주는 곳
+    <div>
+      <p>mainPage테스트</p>
+    </div>
+  );
+}
+
+export default Mainpage;
