f6e0d6a4de92c57b440fb556a1f5f32395bfb918
teawon <xodnjsvkvk9@gmail.com>
2022-08-29 22:30:06 +0900

    chore : install react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Mainpage from "./pages/Mainpage";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <Mainpage />
-    </div>
+    <Routes>
+      <Route path="/" element={<Mainpage />} />
+    </Routes>
   );
 }
 
