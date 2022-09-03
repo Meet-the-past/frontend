5ce0cd560f9a65ce11337622efaf5af1f420e092
teawon <xodnjsvkvk9@gmail.com>
2022-09-04 03:22:23 +0900

    feat : registerPage publishing

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -3,10 +3,12 @@ import CommonNavbar from "../components/CommonNavbar";
 import { Link } from "react-router-dom";
 import InputBox from "../components/InputBox";
 
-function LoginPage() {
+function RegisterPage() {
   const [userInfo, setUserInfo] = useState({
     email: "",
+    userName: "",
     password: "",
+    checkPassword: "",
   });
 
   const handleChange = (e: any) => {
@@ -21,7 +23,7 @@ function LoginPage() {
       <div className="flex justify-center items-center w-full h-screen">
         <div className="shadow-lg w-4/5 h-3/4 rounded-3xl bg-white opacity-80">
           <h1 className="mt-20 text-center text-4xl font-bold text-textColor">
-            로그인
+            회원 가입
           </h1>
 
           <InputBox
@@ -30,6 +32,14 @@ function LoginPage() {
             placeholder="E-mail"
             icon="/assets/images/people-Icon.png"
           />
+
+          <InputBox
+            text={userInfo.userName}
+            onChange={handleChange}
+            placeholder="Name"
+            icon="/assets/images/people-Icon.png"
+          />
+
           <InputBox
             text={userInfo.password}
             onChange={handleChange}
@@ -38,19 +48,24 @@ function LoginPage() {
             type="password"
           />
 
+          <InputBox
+            text={userInfo.checkPassword}
+            onChange={handleChange}
+            placeholder="Conform Password"
+            icon="/assets/images/password-Icon2.png"
+            type="password"
+          />
+
           <div className="mt-5 w-full items-center text-center relative flex justify-center align-middle rounded-full">
-            <button className="w-3/5 h-12 border-black border-2  bg-gray-600 hover:bg-gray-400 text-white font-bold  rounded-full">
-              로그인
+            <button className="w-3/5 h-12 border-black border-2  bg-gray-600 hover:bg-gray-400 text-white font-bold rounded-full">
+              회원가입
             </button>
           </div>
 
           <p className="mt-16 text-center text-xl font-bold text-textColor">
-            <span>Not a member yet?</span>
-            <Link to="/register">
-              <span className="text-blue-800 hover:text-blue-500">
-                {" "}
-                Register!
-              </span>
+            <span>Already have account?</span>
+            <Link to="/login">
+              <span className="text-blue-800 hover:text-blue-500"> Login!</span>
             </Link>
           </p>
         </div>
@@ -59,4 +74,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default RegisterPage;
