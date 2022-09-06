97a9c1adbad1f20627fe6db10fa8ff15e80af384
Chaehy <chlee7670@cau.ac.kr>
2022-09-05 22:24:43 -0700

    style: add upload page background

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import CommonNavbar from "../components/CommonNavbar";
-import { Link } from "react-router-dom";
 
 function UploadPage() {
   return (
-    <div className="bg-mainPageBackground">
+    <div className="">
       <CommonNavbar />
-      <div>
-        <img
-          className="w-full h-44"
-          src="/assets/images/banner.png"
-          alt="banner"
-        />
+      <div className="flex bg-[url('../public/assets/images/background-1.png')] w-full h-92">
+        <div
+          className="bg-white rounded-md my-16 mx-12 w-full"
+          style={{ height: 500, color: "#561D1D" }}
+        >
+          <p className="text-center ">이미지를 복원하시겠습니까?</p>
+          <button className="py-2 px-6 text-center">네</button>
+        </div>
       </div>
     </div>
   );
