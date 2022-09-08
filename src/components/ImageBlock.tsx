1d997440ff2df1c103a8f22e9d0bc58a8ffe2343
teawon <xodnjsvkvk9@gmail.com>
2022-09-09 04:53:07 +0900

    feat :  delete pop-up event
    add Image of deleteButton

--- src/components/ImageBlock.tsx
+++ src/components/ImageBlock.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
+
+import xButtonIcon from "../assets/images/xIcon.svg";
 
 interface ImageBlockProps {
   deleteImageFuc: (id: number) => void;
@@ -6,6 +8,26 @@ interface ImageBlockProps {
 }
 
 function ImageBlock({ deleteImageFuc, ImageDto }: ImageBlockProps) {
+  const useConfirm = (message: string, onConfirm: any) => {
+    if (!onConfirm || typeof onConfirm !== "function") {
+      return;
+    }
+
+    const confirmAction = () => {
+      if (window.confirm(message!)) {
+        onConfirm();
+      }
+    };
+
+    return confirmAction;
+  };
+
+  const deleteConfirm = () => {
+    deleteImageFuc(ImageDto.imageId);
+  };
+
+  const confirmDelete = useConfirm("정말 삭제하시겠습니까?", deleteConfirm);
+
   return (
     <div className="relative w-60 h-60" key={ImageDto.imageId}>
       <img
@@ -13,11 +35,11 @@ function ImageBlock({ deleteImageFuc, ImageDto }: ImageBlockProps) {
         alt="sample"
         src={ImageDto.after_url}
       />
-      <button onClick={() => deleteImageFuc(ImageDto.imageId)}>
+      <button onClick={confirmDelete}>
         <img
           className="absolute w-8 h-8 right-0 top-0"
           alt="deleteBtn"
-          src="/images/negative.png"
+          src={xButtonIcon}
         />
       </button>
     </div>
