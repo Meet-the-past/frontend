6d8d39e4224ce64ad24990c23a01c0bdd18aad58
teawon <xodnjsvkvk9@gmail.com>
2022-09-12 23:22:41 +0900

    fix : AuthHookError

--- src/utils/AuthRoute.tsx
+++ src/utils/AuthRoute.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
-import CheckToken from "./CheckToken";
+import { CheckAccessToken, RequestRefreshToken } from "./CheckToken";
+import { useSelector, useDispatch } from "react-redux";
+import { log_out, login_sucess } from "../redux/reducers/AuthReducer";
 
 interface AuthRouteProps {
   option: boolean;
@@ -9,23 +11,38 @@ interface AuthRouteProps {
 // false => 로그인한 유저는 출입 불가능
 
 export function AuthRoute({ option }: AuthRouteProps) {
+  const dispatch = useDispatch();
+  const authData = useSelector((state: any) => state.Auth);
   const navigate = useNavigate();
-  console.log("cehck");
+
   useEffect(() => {
     (async () => {
-      if (CheckToken()) {
-        console.log("cehck");
-        if (option) {
-          //option이 true일때 로그인으로 강제 이동
-          window.alert("로그인이 만료되었습니다.");
-          navigate("/login");
-        }
-      } else {
-        //로그인을 했을때
+      if (CheckAccessToken(authData)) {
+        console.log("accessToken만료 X");
         if (!option) {
           navigate("/");
           window.alert("비정상적인 접근 입니다.");
         }
+      } else {
+        console.log("만료");
+        //accessToken이 만료
+        RequestRefreshToken(authData)
+          .then((response) => {
+            console.log(response);
+            dispatch(login_sucess(response.data));
+            if (!option) {
+              navigate("/");
+              window.alert("비정상적인 접근 입니다.");
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            dispatch(log_out());
+            if (option) {
+              navigate("/login");
+              window.alert("로그인이 필요합니다.");
+            }
+          });
       }
     })();
   }, []);
--- src/utils/CheckToken.ts
+++ src/utils/CheckToken.ts
@@ -1,43 +1,25 @@
-import { useSelector, useDispatch } from "react-redux";
-import { log_out, login_sucess } from "../redux/reducers/AuthReducer";
 import axios from "./axios";
 
-export default function CheckToken() {
-  const dispatch = useDispatch();
-  const authData = useSelector((state: any) => state.Auth);
-
-  if (authData.accessToken) {
-    //토큰정보가 있다면 만료시간 확인
+const CheckAccessToken = (tokenData: any) => {
+  if (tokenData.accessToken) {
     const currentTime = new Date().getTime() / 1000;
-    console.log(1);
-    if (authData.expiredTime > currentTime + 1000000000000000) {
-      console.log(2);
-      return false;
+
+    if (tokenData.expiredTime > currentTime + 10000) {
+      return true;
     }
-    console.log(3);
-    axios({
-      method: "post",
-      url: `auth/reissue`,
-      data: {
-        accessToken: authData.accessToken,
-        refreshToken: authData.refreshToken,
-      },
-    })
-      .then(function (response) {
-        console.log(4);
-        dispatch(login_sucess(response.data));
-        console.log("토큰 갱신 처리 완료");
-        return false;
-      })
-      .catch(function (error) {
-        console.log(5);
-        console.error("refresh토큰 만료");
-      });
-
-    console.log(6);
-    dispatch(log_out());
-    return true;
+    return false;
   }
+};
 
-  return true;
-}
+const RequestRefreshToken = async (tokenData: any) => {
+  return await axios({
+    method: "post",
+    url: `auth/reissue`,
+    data: {
+      accessToken: tokenData.accessToken,
+      refreshToken: tokenData.refreshToken,
+    },
+  });
+};
+
+export { CheckAccessToken, RequestRefreshToken };
