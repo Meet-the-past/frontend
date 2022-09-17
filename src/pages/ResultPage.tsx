6ba952645eec042dd57c01e1ead95812cde90df0
teawon <xodnjsvkvk9@gmail.com>
2022-09-17 20:10:07 +0900

    feat : ImageModal

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
 import CommonNavbar from "../components/CommonNavbar";
 import Loading from "../components/Loading";
+import Modal from "components/Modal";
 
 function ResultPage() {
   const [isLoding, setIsLoading] = useState<boolean>(true); // AI처리 로딩 여부
   const [imgData, setImgData] = useState<any>();
+  const [openModal, setOpenModal] = useState<boolean>(false);
+  const [selectedImage, setSelectedImage] = useState<string>("");
 
   const downloadImage = (url: any) => {
     fetch(url, { method: "GET" })
@@ -26,6 +29,11 @@ function ResultPage() {
       .catch((err) => {
         console.error("err: ", err);
       });
+  };
+
+  const clickEvent = (url: any) => {
+    setOpenModal(true);
+    setSelectedImage(url);
   };
 
   useEffect(() => {
@@ -54,35 +62,48 @@ function ResultPage() {
           {isLoding ? (
             <Loading text="데이터를 처리중입니다..." />
           ) : (
-            <div className="relative">
-              <div className="pr-32 float-left">
-                <img
-                  className="borderImage flex resultImageSize object-cover object-center aspect-square"
-                  alt="sample"
-                  src={imgData.origin_url}
-                />
-              </div>
-              <div className="pl-32 float-right">
-                <img
-                  className="borderImage flex resultImageSize object-cover object-center aspect-square"
-                  alt="sample"
-                  src={imgData.after_url}
-                />
-                <button
-                  className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
-                  onClick={() => downloadImage(imgData.after_url)}
-                >
-                  <svg
-                    className="fill-current w-4 h-4 mr-2"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
+            <>
+              {openModal && (
+                <Modal closeModal={() => setOpenModal(!openModal)}>
+                  <div className="mt-10 flex justify-center items-center">
+                    <img className="" alt="sample" src={imgData.origin_url} />
+                  </div>
+                  <button
+                    className="mt-20 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+                    onClick={() => downloadImage(imgData.after_url)}
                   >
-                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                  </svg>
-                  <span>Download</span>
-                </button>
+                    <svg
+                      className="fill-current w-4 h-4 mr-2"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                    </svg>
+                    <span>Download</span>
+                  </button>
+                </Modal>
+              )}
+
+              <div className="relative">
+                <div
+                  className="pr-32 float-left"
+                  onClick={() => clickEvent(imgData.after_url)}
+                >
+                  <img
+                    className="borderImage flex resultImageSize object-cover object-center aspect-square"
+                    alt="sample"
+                    src={imgData.origin_url}
+                  />
+                </div>
+                <div className="pl-32 float-right">
+                  <img
+                    className="borderImage flex resultImageSize object-cover object-center aspect-square"
+                    alt="sample"
+                    src={imgData.after_url}
+                  />
+                </div>
               </div>
-            </div>
+            </>
           )}
         </div>
       </div>
